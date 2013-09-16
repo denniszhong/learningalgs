b681e9fb23e8d98daf714f1c13cbce69b1a0f15d
Dennis <denniszhong@gmail.com>
2013-09-17 07:19:23 +0800

    Change printf_s to printf.

--- problems/array/MoveSubarray.cpp
+++ problems/array/MoveSubarray.cpp
@@ -1,5 +1,6 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "stdio.h"
+//#include <stdlib.h>
+
 
 void ReverseArray(int arr[], int start, int end)
 {
@@ -19,12 +20,12 @@ void ReverseArray(int arr[], int start, int end)
 void OutputArray(int arr[], int length)
 {
 	int i = 0;
-	printf_s("Array: ");
+	printf("Array: ");
 	for (i = 0; i < length; i++)
 	{
-		printf_s("%d ", arr[i]);
+		printf("%d ", arr[i]);
 	}
-	printf_s("\n");
+	printf("\n");
 }
 
 /*
@@ -34,30 +35,30 @@ void MoveSubArray(int arr[], int length, int index)
 {
 	if (length <= 0 || index < 0 || index >= length)
 	{
-		printf_s("Array length invalid, or input parameters invalid...\n");
+		printf("Array length invalid, or input parameters invalid...\n");
 		return;
 	}
 
 	if (length == 1)
 	{
-		printf_s("Array contains just one item, skipping move operation...\n");
+		printf("Array contains just one item, skipping move operation...\n");
 		return;
 	}
 
 	if (index == 0 || index == length - 1)
 	{
-		printf_s("Index located at top or tail, skipping move operation...\n");
+		printf("Index located at top or tail, skipping move operation...\n");
 		return;
 	}
 
-	printf_s("Before moving sub-array...\n");
+	printf("Before moving sub-array...\n");
 	OutputArray(arr, length);
 	
 	ReverseArray(arr, 0, index);
 	ReverseArray(arr, index + 1, length - 1);
 	ReverseArray(arr, 0, length - 1);
 
-	printf_s("After moving sub-array...\n");
+	printf("After moving sub-array...\n");
 	OutputArray(arr, length);
 }
 
